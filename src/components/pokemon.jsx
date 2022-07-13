7ea7ab7be2684b6c9cdaebfc78d076c60053a959
Kaijapen <104542328+Kaijapen@users.noreply.github.com>
2022-07-13 08:59:10 -0700

    initial commit

--- src/components/pokemon.jsx
+++ src/components/pokemon.jsx
@@ -1,31 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Pokemon = () => {
+    const [onePokemon, setOnePokemon] = useState('');
     const [pokemon, setPokemon] = useState([]);
+    const [submitAll, setSubmittedAll] = useState(false);
 
-    // useEffect(() => {
-    //     fetch('https://pokeapi.co/api/v2/')
-    //         .then(response => response.json())
-    //         .then(response => setPokemon(response.results))
-    // }, [submit]);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+    }
 
     const pokemon_list = (e) => {
         e.preventDefault()
-        fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
-            .then(response => response.json())
-            .then(response => setPokemon(response.results))
+        setSubmittedAll(true)
     }
 
+    useEffect(() =>{
+        axios.get('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
+            .then(response => {setPokemon(response.data.results)})
+    }, [submitAll]);
+    
     return(
         <div>
-            <form>
-                <button onClick={pokemon_list} type='submit'>Fetch those pokemon!</button>
+            <h1>Let's catch some pokemon</h1>
+            <form onSubmit={handleSubmit} className='w-50 mx-auto'>
+                <div className='d-flex align-items-center'>
+                    <label htmlFor='name' className='form-label me-3'>Pokemon Name:</label>
+                    <input type="text" name='name' className='form-control w-75'/>
+                </div>
+                <div className='d-flex align-items-end flex-column'>
+                    <button className='btn btn-primary' type='submit'>Display One Pokemon</button>
+                    <button className='btn btn-success' onClick={pokemon_list}>Fetch ALL pokemon!</button>
+                </div>
             </form>
             {
-                pokemon.length > 0 ? 
+                submitAll ? 
                     pokemon.map((item, i) =>{
                         return(
-                            <ul>
+                            <ul className='w-25 mx-auto'>
                                 <li key={i}>{item.name}</li>
                             </ul>
                         );
